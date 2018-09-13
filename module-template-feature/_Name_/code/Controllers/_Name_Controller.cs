f190ed878a60cd1b9980ead594b84b0ce8bbd17e
Ole Kristian Hvaale <Ole.kristian@hvaale.net>
2018-09-13 18:36:21 +0200

    first

---
+++ module-template-feature/_Name_/code/Controllers/_Name_Controller.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Web.Mvc;
+using Glass.Mapper.Sc.Web.Mvc;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+
+
+namespace _NamespacePrefix_._ModuleType_._Name_.Controllers
+{
+  public class _Name_Controller : GlassController
+  {
+
+    public ActionResult _Name_()
+    {
+       
+	   return View();
+   
+    }
+  }
+}
---
+++ module-template-feature/_Name_/code/Interfaces/_Name_.cs
@@ -0,0 +1,17 @@
+﻿using System.Web;
+using Glass.Mapper.Sc.Configuration.Attributes;
+using MyHelix.Foundation.GlassMapper.Interfaces;
+
+namespace _NamespacePrefix_._ModuleType_._Name_.Interfaces
+{
+	[SitecoreType]
+  public interface I_Name_ : IGlassBase
+  {
+    // TODO: Implement model or otherwise remove this file
+	
+	    [SitecoreField]
+        string Title { get; set; }
+        [SitecoreField]
+        string Text { get; set; }
+  }
+}
